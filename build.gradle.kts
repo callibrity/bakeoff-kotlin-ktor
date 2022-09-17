f17be7662d07089ef48ea034dcbb7fdd22c51352
James Carman <jcarman@callibrity.com>
2022-09-17 06:33:41 -0400

    Upgrading dependencies and http engine.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 group = "com.callibrity"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("com.callibrity.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
